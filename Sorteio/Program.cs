bbcfec1800290c764bac1afaf567ea8985615201
Renato Lombardo <renato.lombardo@gmail.com>
2020-04-12 20:21:23 -0300

    Adicionar arquivos de projeto.

---
+++ Sorteio/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Sorteio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantidade de sorteios: ");
+            var quantidade = Convert.ToUInt64(Console.ReadLine());
+
+            Shuffle(quantidade);
+        }
+
+        private static void Shuffle(ulong numberOfTries)
+        {
+            int correctHits = 0;
+            int wrongHits = 0;
+
+            var prizeIndex = new Random();
+
+            for (ulong i = 1; i <= numberOfTries; i++)
+            {
+                int[] doors = { 1, 2, 3 };
+
+                int prizeDoor = doors[prizeIndex.Next(0, doors.Length)] ;
+
+                int selectedDoor = doors[prizeIndex.Next(0, doors.Length)];               
+
+                int discardedDoor = doors[prizeIndex.Next(0, doors.Length)];
+
+                while (discardedDoor == prizeDoor || discardedDoor == selectedDoor)
+                {
+                    discardedDoor = doors[prizeIndex.Next(0, doors.Length)];
+                }
+
+                var correctGuess = selectedDoor == prizeDoor;
+
+                if (correctGuess) correctHits++;
+                else wrongHits++;
+
+                Console.WriteLine($"Prize Door: {prizeDoor}\nSelected Door: {selectedDoor}\nDiscarded Door: {discardedDoor}\nRight Guess? {correctGuess}\n\n");
+            }
+
+            Console.WriteLine($"Right Guesses: {correctHits} / {numberOfTries}\nWrong Guesses: {wrongHits} / {numberOfTries}");
+        }
+    }
+}
